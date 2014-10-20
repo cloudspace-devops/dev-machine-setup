2d55ca6eb5871ec04a090c91e9e1b0077e0560e5
Scott Moe <smoe868@gmail.com>
2014-10-20 11:28:22 -0400

    Change homebrew install script location

--- osx-dev-machine-setup.sh
+++ osx-dev-machine-setup.sh
@@ -4,7 +4,7 @@
 xcode-select --install
 
 # Install Brew
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap homebrew/binary
 brew update
 
